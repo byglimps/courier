c32281699586b34a1c7e8b5770e3d6227f7a63a7
Juan Alvarado <jalvarado91@gmail.com>
2018-09-03 19:38:07 -0400

    Conects to messaging service
    - Adds api,service,controller to send glimps over text using the courier service
    - Fixes issue with creator middleware
    - moves services urls to environment vars

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
-
 import text from "../controllers/text";
-
 import mail from "../controllers/mail";
 
 const apiRouter = Router();
 
 apiRouter.post("/text", text.create);
-
 apiRouter.post("/mail", mail.create);
 
 export default apiRouter;
